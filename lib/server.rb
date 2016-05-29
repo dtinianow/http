1165a2b5bb9984619022f0a6599cabed7cf4bad8
David Tinianow <david.tinianow@gmail.com>
2016-05-29 17:21:07 -0600

    working through iteration 1

--- lib/server.rb
+++ lib/server.rb
@@ -3,28 +3,36 @@ require 'pry'
 
 class Server
 
-attr_reader :tcp_server, :client
+attr_reader :tcp_server, :client, :hello_count
 
   def initialize
     @tcp_server = TCPServer.new(9292)
     @hello_count = 0
   end
 
+  # def request_lines(client)
+  # request_lines = []
+  #   while line = client.gets and !line.chomp.empty?
+  #     request_lines << line.chomp
+  #   end
+  # end
+
   def response
     loop do
       client = tcp_server.accept
       request_lines = []
-      while line = client.gets and !line.chomp.empty?
-        request_lines << line.chomp
-      end
-
+        while line = client.gets and !line.chomp.empty?
+          request_lines << line.chomp
+        end
       client.puts "Hello world (#{@hello_count})\n"
       client.puts request_lines.join("\n")
       client.close
       @hello_count += 1
     end
   end
+
 end
+
 
 server = Server.new
 server.response
--- test/server_test.rb
+++ test/server_test.rb
@@ -4,9 +4,26 @@ require './lib/server'
 
 class ServerTest < Minitest::Test
 
+  def test_server_exists
+    server = Server.new
+    assert_instance_of Server, server
+  end
 
+  def test_tcp_server_exists
+    tcp_server = TCPServer.new(9000)
+    assert_instance_of TCPServer, tcp_server
+  end
 
+  def test_can_keep_track_of_hello
+    server = Server.new
 
+    assert_equal 0, server.hello_count
+    # server.response
+    # assert_equal 1, server.hello_count
+    # server.response
+    # refute_equal 1, server.hello_count
+    # assert_equal 2, server.hello_count
+  end
 
 
 end
