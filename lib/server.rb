185ffed05916b27c76c95336e57a3de92c65915d
David Tinianow <david.tinianow@gmail.com>
2016-05-30 13:33:02 -0600

    iteration_1 revised

--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'pry'
+require './lib/parser'
 
 class Server
 
@@ -10,10 +11,11 @@ attr_reader :tcp_server, :hello_count
     @hello_count   = 0
   end
 
-  def response
+  def start
     while client = tcp_server.accept
       request = get_request(client)
-      output_client_messages(client, request)
+      @parsed_message = Parser.new(request)
+      output_client_messages(client, @parsed_message)
       @hello_count += 1
     end
   end
@@ -26,9 +28,14 @@ attr_reader :tcp_server, :hello_count
     request_lines
   end
 
-  def output_client_messages(client, request)
+  def output_client_messages(client, parsed_message)
     client.puts "Hello world (#{@hello_count})\n"
-    client.puts request.join("\n")
+    # client.puts request.join("\n")
+    client.puts @parsed_message.first_request_line
+    client.puts @parsed_message.remaining_request_lines
+    client.puts @parsed_message.last_request_line
+    client.puts "\n"
+    client.puts @parsed_message.request
     client.close
   end
 
@@ -36,4 +43,4 @@ end
 
 
 server = Server.new
-server.response
+server.start
---
+++ test/parser_test.rb
@@ -0,0 +1,16 @@
+require './test/testhelper'
+require './lib/server'
+require './lib/parser'
+
+class ParserTest < Minitest::Test
+  #
+  # def test_parser_exists
+  #   parser = Parser.new()
+  #   assert_instance_of Parser, parser
+  # end
+
+  # def test_first_request_line_has_three_elements
+  #
+  # end
+
+end
