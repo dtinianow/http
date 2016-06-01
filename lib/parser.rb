c6025481f7ce8d26709df0762a95b15f07555d72
David Tinianow <david.tinianow@gmail.com>
2016-05-31 19:45:34 -0600

    experimenting with parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,17 +1,14 @@
 
 class Parser
 
-  attr_reader :request, :all_request_lines, :path
-
+  attr_reader :request, :request_info, :all_request_lines, :path
 
   def initialize(request)
     @request       = request
     @path          = request[0].split(" ")[1].split('?')[0]
-    @request_lines = {}
+    @request_info = {}
   end
 
-  #if request[0].split(" ")[1].include?('?')
-  # @path = path?param=value
   def word
     if request[0].split(" ")[1].split('?')[1].split('=')[0] == 'word'
       word = request[0].split(" ")[1].split('?')[1].split('=')[1]
@@ -19,28 +16,62 @@ class Parser
     word  #redirect if word not there. #rubular
   end
 
-  def first_request_line
-    first_line = request[0].split(" ")
-    "Verb: #{first_line[0]}\nPath: #{first_line[1]}\nProtocol: #{first_line[2]}"
+  def request_info
+    get_verb
+    get_root
+    get_protocol
+    get_host
+    get_port
+    get_origin
+    get_accept
   end
 
-  def remaining_request_lines
-    second_line = request[1].delete(" ").split(":")
-    "Host: #{second_line[1]}\nPort: #{second_line[2]}\nOrigin: #{second_line[1]}"
+  def get_verb
+    request_info["Verb:"] = request[0].split(" ")[0]
   end
 
-  def last_request_line
-    last_line = request[2].split(" ")
-    "Accept: #{last_line[1]}"
+  def get_root
+    request_info["Path:"] = request[0].split(" ")[1]
   end
+
+  def get_protocol
+    request_info["Protocol:"] = request[0].split(" ")[2]
+  end
+
+  def get_host
+    request_info["Host:"] = request[1].delete(" ").split(":")[1]
+  end
+
+  def get_port
+    request_info["Port:"] = request[1].delete(" ").split(":")[2]
+  end
+
+  def get_origin
+    request_info["Origin:"] = request[1].delete(" ").split(":")[1]
+  end
+
+  def get_accept
+    request_info["Accept:"] = request[2].split(" ")[1]
+  end
+
+  # def first_request_line
+  #   first_line = request[0].split(" ")
+  #   "Verb: #{first_line[0]}\nPath: #{first_line[1]}\nProtocol: #{first_line[2]}"
+  # end
+  #
+  # def remaining_request_lines
+  #   second_line = request[1].delete(" ").split(":")
+  #   "Host: #{second_line[1]}\nPort: #{second_line[2]}\nOrigin: #{second_line[1]}"
+  # end
+  #
+  # def last_request_line
+  #   last_line = request[2].split(" ")
+  #   "Accept: #{last_line[1]}"
+  # end
 
   def all_request_lines
-    "#{first_request_line}\n#{remaining_request_lines}\n#{last_request_line}"
+    lines = individual_requests.each do
+    # "#{first_request_line}\n#{remaining_request_lines}\n#{last_request_line}"
   end
 
 end
-
-
-# def get_verb
-#   @request_lines["Verb:"] = request[0].split(" ")[0]
-# end
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -3,93 +3,70 @@ require './lib/server'
 require './lib/parser'
 
 class ParserTest < Minitest::Test
-  # def request
-  #
-  # end
-  #
-  # def setup
-  #   @parser = Parser.new
-  # end
-  #
-  # def test_it_defined_verb
-  #   assert_equal "GET", @parser.verb
-  # end
+
+  attr_reader :request, :parser
+
+  def setup
+    @request = ["GET / HTTP/1.1",
+               "Host: 127.0.0.1:9292",
+               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"]
+    @parser = Parser.new(request)
+  end
 
   def test_parser_exists
-    request = ["GET / HTTP/1.1",
-               "Host: 127.0.0.1:9292",
-               "Connection: keep-alive",
-               "Cache-Control: no-cache",
-               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-               "Postman-Token: 3c37014c-ab5c-20d6-1a7c-ddcd5c01339c",
-               "Accept: */*",
-               "Accept-Encoding: gzip, deflate, sdch",
-               "Accept-Language: en-US,en;q=0.8"]
-    parser = Parser.new(request)
     assert_instance_of Parser, parser
   end
 
   def test_can_split_first_request_line
-    request = ["GET / HTTP/1.1",
-               "Host: 127.0.0.1:9292",
-               "Connection: keep-alive",
-               "Cache-Control: no-cache",
-               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-               "Postman-Token: 3c37014c-ab5c-20d6-1a7c-ddcd5c01339c",
-               "Accept: */*",
-               "Accept-Encoding: gzip, deflate, sdch",
-               "Accept-Language: en-US,en;q=0.8"]
-    parser = Parser.new(request)
     expected = "Verb: GET\nPath: /\nProtocol: HTTP/1.1"
     assert_equal expected, parser.first_request_line
   end
 
   def test_can_split_middle_request_lines
-    request = ["GET / HTTP/1.1",
-               "Host: 127.0.0.1:9292",
-               "Connection: keep-alive",
-               "Cache-Control: no-cache",
-               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-               "Postman-Token: 3c37014c-ab5c-20d6-1a7c-ddcd5c01339c",
-               "Accept: */*",
-               "Accept-Encoding: gzip, deflate, sdch",
-               "Accept-Language: en-US,en;q=0.8"]
-    parser = Parser.new(request)
     expected = "Host: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1"
     assert_equal expected, parser.remaining_request_lines
   end
 
   def test_can_split_last_request_line
-    request = ["GET / HTTP/1.1",
-               "Host: 127.0.0.1:9292",
-               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-               "Connection: keep-alive",
-               "Cache-Control: no-cache",
-               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-               "Postman-Token: 3c37014c-ab5c-20d6-1a7c-ddcd5c01339c",
-               "Accept: */*",
-               "Accept-Encoding: gzip, deflate, sdch",
-               "Accept-Language: en-US,en;q=0.8"]
-    parser = Parser.new(request)
     expected = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     assert_equal expected, parser.last_request_line
   end
 
   def test_can_combine_all_modified_request_lines
-    request = ["GET / HTTP/1.1",
-               "Host: 127.0.0.1:9292",
-               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"]
-    parser = Parser.new(request)
     expected = "Verb: GET\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     assert_equal expected, parser.all_request_lines
   end
 
-  # def test_can_get_verb
-  #   request = ["GET / HTTP/1.1",
-  #              "Host: 127.0.0.1:9292",
-  #              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"]
-  #   parser = Parser.new(request)
-  #   assert_equal 'GET', parser.get_verb
-  # end
+  def test_can_get_verb
+    assert_equal 'GET', parser.get_verb
+  end
+
+  def test_can_get_root_path
+    assert_equal '/', parser.get_root
+  end
+
+  def test_can_get_protocol
+    assert_equal 'HTTP/1.1', parser.get_protocol
+  end
+
+  def test_can_get_host
+    assert_equal '127.0.0.1', parser.get_host
+  end
+
+  def test_can_get_port
+    assert_equal '9292', parser.get_port
+  end
+
+  def test_can_get_origin
+    assert_equal "127.0.0.1", parser.get_origin
+  end
+
+  def test_can_get_accept
+    assert_equal 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', parser.get_accept
+  end
+
+  def test_request_hash
+    assert_equal "thing", parser.request_hash
+  end
 
 end
